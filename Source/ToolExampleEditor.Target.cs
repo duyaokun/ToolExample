ae260ddfdb27965e22204b44e534b032b4ee32e0
duyaokun <duyaokun@foxmail.com>
2024-01-21 00:05:37 +0800

    New defaults for 4.24 "DefaultBuildSettings = BuildSettingsVersion.V2"

--- Source/ToolExampleEditor.Target.cs
+++ Source/ToolExampleEditor.Target.cs
@@ -8,6 +8,11 @@ public class ToolExampleEditorTarget : TargetRules
 	public ToolExampleEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+#if UE_5_2_OR_LATER
+		DefaultBuildSettings = BuildSettingsVersion.V3;
+#elif UE_4_24_OR_LATER
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+#endif
 		ExtraModuleNames.AddRange( 
 			new string[] {
 				"ToolExample",
